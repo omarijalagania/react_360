1dab6130827422b0da7ec7eb3bceaac446207cbe
Irakli <koregm@gmail.com>
2021-09-14 18:56:19 +0400

    delete test console log

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-console.log("test")
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
